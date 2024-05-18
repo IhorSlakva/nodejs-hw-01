10416905998173b1df3ac5af3c9898c56c441dc6
IhorSlakva <igorek.slakva@gmail.com>
2024-05-18 11:59:29 +0300

    update

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    const emptyContacts = [];
+    await fs.writeFile(PATH_DB, JSON.stringify(emptyContacts));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 await removeAllContacts();
